a624b1de7ea428b75ccea8965a0f17c489b77e92
Juan Manuel de Palacios <juanma36@gmail.com>
2020-05-30 16:31:20 -0400

    design

--- src/components/App/Calculator.js
+++ src/components/App/Calculator.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import "./Calculator.css";
+
 const Calculator = () => {
-    return <div className = "Calculator">Hello Calculator</div>;
+// building the calculator structure
+    return <div className = "Calculator">
+        <h1>23PEOPLE CALCULATOR</h1>
+        <div className = "display">0</div>
+        <div className = "buttonAC">AC</div>
+        <div className = "buttom">-</div>
+    </div>;
 };
 
 export default Calculator;
