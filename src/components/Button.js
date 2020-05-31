1e7c09633e556acb468eb1d5d1faa72507a0ca9a
Juan Manuel de Palacios <juanma36@gmail.com>
2020-05-31 16:09:27 -0400

    adding functionalities

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,8 +3,12 @@ import React from "react";
 import "./Button.css";
 
 //button values ​​and hood the equal
-const Button = ({ value, type }) => {
-  return <div className={`Button ${value=== '=' ? 'equal' : ''} ${type || ""}`}> {value} </div>;
+const Button = ({ value, onClickButton, type }) => {
+  return (
+    <div className={`Button ${value === "=" ? "equal" : ""} ${type || ""}`} onClick={onClickButton(value)}>
+      {value}
+    </div>
+  );
 };
 
 export default Button;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -4,32 +4,48 @@ import Button from "./Button";
 import "./Calculator.css";
 
 const Calculator = () => {
-  const[target, setTarget] = useState("123");
+  const [target, setTarget] = useState("0");
+
+  const handleButton = (value) => () => {
+    const num = parseFloat(value);
+
+    if (value === "AC") {
+      setTarget("0");
+      return;
+    }
+    if (value === "±") {
+        setTarget(( num * -1).toString());
+        return;
+      }
+    
+
+    setTarget(parseFloat(num + value).toString());
+  };
   // building the calculator structure
   return (
     <div className="Calculator">
       <h1>23PEOPLE CALCULATOR</h1>
       <div className="display">{target}</div>
       <div className="buttons">
-        <Button value="AC"type="function"/>
-        <Button value="±" type="function"/>
-        <Button value="%" type="function"/>
-        <Button value="/" type="operator"/>
-        <Button value="7" />
-        <Button value="8" />
-        <Button value="9" />
-        <Button value="x" type="operator"/>
-        <Button value="4" />
-        <Button value="5" />
-        <Button value="6" />
-        <Button value="-" type="operator"/>
-        <Button value="1" />
-        <Button value="2" />
-        <Button value="3" />
-        <Button value="+" type="operator"/>
-        <Button value="0" />
-        <Button value="," />
-        <Button value="=" type="operator"/>
+        <Button onClickButton={handleButton} value="AC" type="function" />
+        <Button onClickButton={handleButton} value="±" type="function" />
+        <Button onClickButton={handleButton} value="%" type="function" />
+        <Button onClickButton={handleButton} value="÷" type="operator" />
+        <Button onClickButton={handleButton} value="7" />
+        <Button onClickButton={handleButton} value="8" />
+        <Button onClickButton={handleButton} value="9" />
+        <Button onClickButton={handleButton} value="x" type="operator" />
+        <Button onClickButton={handleButton} value="4" />
+        <Button onClickButton={handleButton} value="5" />
+        <Button onClickButton={handleButton} value="6" />
+        <Button onClickButton={handleButton} value="-" type="operator" />
+        <Button onClickButton={handleButton} value="1" />
+        <Button onClickButton={handleButton} value="2" />
+        <Button onClickButton={handleButton} value="3" />
+        <Button onClickButton={handleButton} value="+" type="operator" />
+        <Button onClickButton={handleButton} value="0" />
+        <Button onClickButton={handleButton} value="," />
+        <Button onClickButton={handleButton} value="=" type="operator" />
       </div>
       <div className="buttom">-</div>
     </div>
